2bf03f6c7692473f4f8d8dfeb036490147272b40
Chem Info <j.weiszmann@gmail.com>
2016-09-21 16:24:45 +0200

    initial commit

---
+++ doit.py
@@ -0,0 +1,22 @@
+strat = (addSubset(inputGraphs) >> inputRules)
+dg = dgRuleComp(inputGraphs, strat)
+dg.calc()
+dg.print()
+
+postSection("Input Molecules")
+for m in inputGraphs:
+    m.print()
+
+postSection("Rule")
+for r in inputRules:
+    r.print
+
+postSection("Product Molecules")
+for m in dg.products:
+    m.print
+    if m.isMolecule:
+        print(m.smiles)
+
+postSection("Derivations")
+for dRef in dg.derivations:
+    dRef.print()
